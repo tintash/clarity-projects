813963c2ae8bc0604671c19a0c60cfe5df2ed022
arsalan <khanaarsalan007@gmail.com>
2021-08-02 13:37:53 +0500

    done changes suggested by  Faried: included the flow of seller and buyer both transfering STX to the escrow, sending remainging STX to both parties after the transaction completes

--- 001-escrow/clarinet-projects/arsalankhan/contracts/escrow.clar
+++ 001-escrow/clarinet-projects/arsalankhan/contracts/escrow.clar
@@ -1,28 +1,41 @@
 (define-data-var seller (optional principal) none)
 (define-data-var buyer (optional principal) none)
 (define-data-var item-amount uint u0)
+(define-data-var seller-amount uint u0)
 
 (define-constant err-buyer-only (err u100))
+(define-constant err-seller-only (err u101))
 
-(define-public (send-funds (amount uint) (recipient principal))
-(begin
-    (var-set item-amount amount)
+(define-public (buyItem (amount uint) (recipient principal))
+  (begin
+    (var-set item-amount (/ amount u2))
     (var-set buyer (some tx-sender))
     (var-set seller (some recipient))
     (try! (stx-transfer? amount tx-sender (as-contract tx-sender)))
     (ok true)
-)
+  )
 )
 
-(define-private (payout-balance)
-  (unwrap-panic (as-contract (stx-transfer? (var-get item-amount) tx-sender (unwrap-panic (var-get seller)))))
+(define-public (transferFunds (amount uint))
+  (begin
+    (asserts! (is-eq (some tx-sender) (var-get seller)) err-seller-only)
+    (try! (stx-transfer? amount tx-sender (as-contract tx-sender)))
+    (var-set seller-amount amount)
+    (ok true)
+  )
+)
+
+(define-private (payoutBalance)
+  (begin
+    (unwrap-panic (as-contract (stx-transfer? (+ (var-get item-amount) (var-get seller-amount)) tx-sender (unwrap-panic (var-get seller)))))
+    (unwrap-panic (as-contract (stx-transfer? (var-get item-amount) tx-sender (unwrap-panic (var-get seller)))))
+  )
 )
 
 (define-public (itemRecieved)
-(begin
+  (begin
     (asserts! (is-eq (some tx-sender) (var-get buyer)) err-buyer-only)
-    (payout-balance)
+    (payoutBalance)
     (ok true)
-)
-
+  )
 )
--- 001-escrow/clarinet-projects/arsalankhan/tests/escrow_test.ts
+++ 001-escrow/clarinet-projects/arsalankhan/tests/escrow_test.ts
@@ -3,41 +3,51 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that funds transfered to the escrow contract",
+    name: "Ensure that funds transfered to the escrow contract by buyer and seller",
     async fn(chain: Chain, accounts: Map<string, Account>) {
 
         const buyer = accounts.get('wallet_1')!;
         const seller = accounts.get('wallet_2')!;
         const deployer = accounts.get('deployer')!  ;
         const amount = 100;
-        console.log(buyer);
         let block = chain.mineBlock([
-            Tx.contractCall('escrow', 'send-funds',[types.uint(amount), types.principal(seller.address)], buyer.address)
-        ]);
+            Tx.contractCall('escrow', 'buyItem',[types.uint(amount), types.principal(seller.address)], buyer.address),
+            Tx.contractCall('escrow', 'transferFunds',[types.uint(amount)], seller.address)
+
+         ]);
         block.receipts[0].result.expectOk().expectBool(true);
         block.receipts[0].events.expectSTXTransferEvent(amount, buyer.address, `${deployer.address}.escrow`);
+        block.receipts[1].result.expectOk().expectBool(true);
+        block.receipts[1].events.expectSTXTransferEvent(amount, seller.address, `${deployer.address}.escrow`);
     },
 });
 
+
 Clarinet.test({
-    name: "Ensure that item received and contrat cleared out the funds to seller",
+    name: "Ensure that item received and contrat cleared out the funds to seller and buyer",
     async fn(chain: Chain, accounts: Map<string, Account>) {
 
         const buyer = accounts.get('wallet_1')!;
         const seller = accounts.get('wallet_2')!;
         const deployer = accounts.get('deployer')!;
         let assetMaps = chain.getAssetsMaps();
-        const balance = assetMaps.assets["STX"][seller.address];
-        const amount = 100;
-        console.log(buyer);
+        const balance_buyer = assetMaps.assets["STX"][buyer.address];
+        const balance_seller = assetMaps.assets["STX"][seller.address];
+        
+        const item_amount = 100;
+        let sellerAmount = item_amount / 2 + item_amount;
 
         let block = chain.mineBlock([
             Tx.contractCall('escrow', 'itemRecieved',[], buyer.address)
         ]);
         block.receipts[0].result.expectOk().expectBool(true);
-        block.receipts[0].events.expectSTXTransferEvent(amount, `${deployer.address}.escrow`, seller.address);
+        block.receipts[0].events.expectSTXTransferEvent(sellerAmount, `${deployer.address}.escrow`, seller.address);
+        block.receipts[0].events.expectSTXTransferEvent(item_amount/2, `${deployer.address}.escrow`, buyer.address);
+
 
         assetMaps = chain.getAssetsMaps();
-        assertEquals(assetMaps.assets["STX"][seller.address], balance + amount);
+        assertEquals(assetMaps.assets["STX"][seller.address], balance_seller + sellerAmount);
+        assertEquals(assetMaps.assets["STX"][seller.address], balance_buyer + item_amount/2);
+
     },
 });
