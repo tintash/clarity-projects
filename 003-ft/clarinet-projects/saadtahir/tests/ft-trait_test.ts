daf46efd2884f727f24051f99e26cf76ede8091a
Saad Tahir <saad.tahir@tintash.com>
2021-08-12 16:54:21 +0500

    removed useless code

--- 003-ft/clarinet-projects/saadtahir/tests/ft-trait_test.ts
+++
@@ -1,8 +0,0 @@
-import {
-  Clarinet,
-  Tx,
-  Chain,
-  Account,
-  types,
-} from "https://deno.land/x/clarinet@v0.14.0/index.ts";
-import { assertEquals } from "https://deno.land/std@0.90.0/testing/asserts.ts";
