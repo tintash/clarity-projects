dbd3081ec16fcfd1a1d6afd9bf66d123132ba2ae
NailaNawaz <nailanawaz@192.168.10.7>
2021-08-24 18:19:25 +0500

    game test added

--- 003-ft/clarinet-projects/NailaNawaz/ft/tests/game_test.ts
+++ 003-ft/clarinet-projects/NailaNawaz/ft/tests/game_test.ts
@@ -3,24 +3,49 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Ensure that player can't play game with not enough coins",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+        const deployer = accounts.get("deployer")!;
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+        let block = chain.mineBlock([
+            Tx.contractCall(
+                "game",
+                "play-game",
+                [],
+                deployer.address
+            )
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+        assertEquals(block.receipts.length, 1);
+        block.receipts[0].result.expectOk().expectBool(false);
+        assertEquals(block.height, 2);
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that player can play game with enough coins",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const player = accounts.get("wallet_1")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall(
+                "game",
+                "register-user",
+                [types.principal(deployer.address)],
+                deployer.address
+            ),
+
+            Tx.contractCall (
+                "game",
+                "play-game",
+                [],
+                deployer.address
+            )
+        ]);
+        assertEquals(block.receipts.length, 2);
+        block.receipts[0].result.expectOk().expectBool(true);
+        assertEquals(block.height, 2);
+        console.log(block.receipts[1]) 
+        block.receipts[1].result.expectOk().expectBool(true);
     },
 });
