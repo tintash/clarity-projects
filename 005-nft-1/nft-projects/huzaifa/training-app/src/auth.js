3781e0c6bb852350b25daa50c78c49ee0419ee15
MoHuzaifa <muhammad.huzaifa@tintash.com>
2021-10-11 16:54:08 +0500

    Add selling page form details

--- 005-nft-1/nft-projects/huzaifa/training-app/src/auth.js
+++ 005-nft-1/nft-projects/huzaifa/training-app/src/auth.js
@@ -1,5 +1,4 @@
 import { AppConfig, UserSession, showConnect } from '@stacks/connect';
-import { Person } from '@stacks/profile';
 
 const appConfig = new AppConfig(['store_write', 'publish_data']);
 
@@ -24,8 +23,4 @@ export function authenticate(setSelected) {
 
 export function getUserData() {
   return userSession.loadUserData();
-}
-
-export function getPerson() {
-  return new Person(getUserData().profile);
 }
--- 005-nft-1/nft-projects/huzaifa/training-app/src/pages/sellingPage/SellingPage.js
+++ 005-nft-1/nft-projects/huzaifa/training-app/src/pages/sellingPage/SellingPage.js
@@ -1,22 +1,34 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-import Table from '../../components/table/Table';
 import NavBar from '../../components/NavBar/NavBar';
 import Footer from '../../components/footer/Footer';
-import { INFO_TABLE_LABELS, INFO_TABLE_VALUES } from '../../constants';
+import CustomButton from '../../components/custom-button/CustomButton';
 import { getUserData } from '../../auth';
 
 import './SellingPage.scss';
 
 const SellingPage = () => {
+  const [tokenId, setTokenId] = useState(0);
+  const [tokenPrice, setTokenPrice] = useState(10000);
+  const [selected, setSelected] = useState(false);
+  const listOfTokens = ['a', 'b', 'c'];
   const { testnet } = getUserData().profile.stxAddress;
   const { mainnet } = getUserData().profile.stxAddress;
-  INFO_TABLE_VALUES[0] = testnet;
-  INFO_TABLE_VALUES[1] = mainnet;
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // setSelected(!selected);
+    alert('Form submitted');
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setTokenId(value);
+  };
 
   return (
     <div className="sell-outer-container">
@@ -24,7 +36,33 @@ const SellingPage = () => {
       <div className="sell-container">
         <div className="sell-info-container fade-in">
           <h1 className="sell-heading">Sell tokens</h1>
-          <Table labels={INFO_TABLE_LABELS} values={INFO_TABLE_VALUES} />
+          <form onSubmit={handleSubmit} className="selling-form">
+            <div className="form-row">
+              <label className="form-label">Token ID</label>
+              <select onChange={handleChange}>
+                <option selected disabled value="">-Select From Tokens-</option>
+                {listOfTokens?.length > 0
+                  ? listOfTokens.map((token) => <option value={token}>{token}</option>)
+                  : ''}
+              </select>
+            </div>
+
+            <div className="form-row">
+              <label className="form-label">
+                Token Price &gt; 10000STX
+              </label>
+              <input
+                type="number"
+                value={tokenPrice}
+                onChange={(s) => setTokenPrice(s.target.value)}
+              />
+            </div>
+
+            <div className="sell-button">
+              <CustomButton selected={selected}>Sell</CustomButton>
+            </div>
+
+          </form>
         </div>
         <div className="sell-tokens-container fade-in">
           <h1 className="sell-tokens-heading">Tokens up for sale</h1>
