55560212530a3cb6c9a772859dbbbd3087720da9
saad-s <saad.shafiq@tintash.com>
2021-11-08 16:51:00 +0500

    ## refactor
    * import constants together

--- 004-ft-interface/backend/saad-s/src/contract-calls.js
+++ 004-ft-interface/backend/saad-s/src/contract-calls.js
@@ -17,19 +17,22 @@ const network = new StacksTestnet();
 var mnemonic =
   "inside claim kick easily assist trim cat silk culture pumpkin drastic claim tail bleak journey lunar nose apple result draw fiscal present unlock evil";
 
-import {
-  REFER_REWARD_CONTRACT,
-  RR_TOKEN_NAME,
-  RR_TOKEN_SYMBOL,
-  RR_TOKEN_DECIMALS,
-  RR_TOKEN_SUPPLY,
-  RR_TOKEN_URI,
-  RR_REFER_USER,
-  RR_PERFORM_TRANSACTION,
-} from "./constants";
+// import {
+//   REFER_REWARD_CONTRACT,
+//   RR_TOKEN_NAME,
+//   RR_TOKEN_SYMBOL,
+//   RR_TOKEN_DECIMALS,
+//   RR_TOKEN_SUPPLY,
+//   RR_TOKEN_URI,
+//   RR_REFER_USER,
+//   RR_PERFORM_TRANSACTION,
+// } from "./constants";
+
+import * as constants from "./constants";
 
 async function makeReadOnlyCall(functionName) {
-  const [contractAddress, contractName] = REFER_REWARD_CONTRACT.split(".");
+  const [contractAddress, contractName] =
+    constants.REFER_REWARD_CONTRACT.split(".");
   const data = await callReadOnlyFunction({
     contractAddress: contractAddress,
     contractName: contractName,
@@ -42,27 +45,27 @@ async function makeReadOnlyCall(functionName) {
 }
 
 export async function getTokenName() {
-  const data = await makeReadOnlyCall(RR_TOKEN_NAME);
+  const data = await makeReadOnlyCall(constants.RR_TOKEN_NAME);
   return cvToJSON(data).value.value;
 }
 
 export async function getTokenSymbol() {
-  const data = await makeReadOnlyCall(RR_TOKEN_SYMBOL);
+  const data = await makeReadOnlyCall(constants.RR_TOKEN_SYMBOL);
   return cvToJSON(data).value.value;
 }
 
 export async function getTokenDecimals() {
-  const data = await makeReadOnlyCall(RR_TOKEN_DECIMALS);
+  const data = await makeReadOnlyCall(constants.RR_TOKEN_DECIMALS);
   return cvToJSON(data).value.value;
 }
 
 export async function getTokenSupply() {
-  const data = await makeReadOnlyCall(RR_TOKEN_SUPPLY);
+  const data = await makeReadOnlyCall(constants.RR_TOKEN_SUPPLY);
   return cvToJSON(data).value.value;
 }
 
 export async function getTokenURI() {
-  const data = await makeReadOnlyCall(RR_TOKEN_URI);
+  const data = await makeReadOnlyCall(constants.RR_TOKEN_URI);
   return cvToJSON(data).value.value.value;
 }
 
@@ -81,11 +84,12 @@ export async function referUser(req) {
   const ecPair = bitcoin.ECPair.fromPrivateKey(child.privateKey);
   const privkey = blockstack.ecPairToHexString(ecPair);
 
-  const [contractAddress, contractName] = REFER_REWARD_CONTRACT.split(".");
+  const [contractAddress, contractName] =
+    constants.REFER_REWARD_CONTRACT.split(".");
   const transaction = await makeContractCall({
     contractAddress: contractAddress,
     contractName: contractName,
-    functionName: RR_REFER_USER,
+    functionName: constants.RR_REFER_USER,
     functionArgs: [newUserEmail, newUserAddress],
     senderKey: privkey,
     validateWithAbi: true,
@@ -103,11 +107,12 @@ export async function completeTransaction() {
   const ecPair = bitcoin.ECPair.fromPrivateKey(child.privateKey);
   const privkey = blockstack.ecPairToHexString(ecPair);
 
-  const [contractAddress, contractName] = REFER_REWARD_CONTRACT.split(".");
+  const [contractAddress, contractName] =
+    constants.REFER_REWARD_CONTRACT.split(".");
   const transaction = await makeContractCall({
     contractAddress: contractAddress,
     contractName: contractName,
-    functionName: RR_PERFORM_TRANSACTION,
+    functionName: constants.RR_PERFORM_TRANSACTION,
     functionArgs: [],
     senderKey: privkey,
     validateWithAbi: true,
